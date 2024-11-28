28259ce2c74ffdc4c0e880b8a5cb25e60da8acd7
doniahmed18 <134319004+doniahmed18@users.noreply.github.com>
2024-11-28 14:10:27 +0200

    login error resolved

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,103 +1,129 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 import "../app/globals.css";
-import Navbar from '../components/Navbar';
-import Link from 'next/link';
-import {message,Modal} from "antd";
-import Lottie from "lottie-react"
-import LoginAnimation from "../../public/Login.json"
+import Navbar from "../components/Navbar";
+import Link from "next/link";
+import Lottie from "lottie-react";
+import LoginAnimation from "../../public/Login.json";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post('http://localhost:3000/auth/login', { email, password });
-      message.success('Login successful!',2);
-      console.log('Token:', response.data.accessToken);
+      const response = await axios.post("http://localhost:3000/auth/login", {
+        email,
+        password,
+      });
+      toast.success("Login successful!");
+      console.log("Token:", response.data.accessToken);
     } catch (error) {
       // + error.response.data.message
-      message.error('Login failed: ',4);
+      toast.error("Login Faailed!");
     }
   };
 
   return (
-    <div style={{ backgroundColor: "#FBFBFB", height: "100vh", display: "flex", flexDirection: "column" }}>
-      <Navbar />
-      <div style={{
+    <div
+      style={{
+        backgroundColor: "#FBFBFB",
+        height: "100vh",
         display: "flex",
-        flexDirection: "row",
-        gap:"50px",
-        alignItems: "center",
-        justifyContent: "center", 
-        height: "100%", 
-        textAlign: "center",
-      }}>
-        <div style={{marginRight:"20px"}}>
-        <Lottie className="h-64" animationData={LoginAnimation} />
+        flexDirection: "column",
+      }}
+    >
+      <Navbar />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "50px",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100%",
+          textAlign: "center",
+          padding:"9px",
 
+        }}
+      >
+        <div style={{ marginRight: "20px" }}>
+          <Lottie className="h-64" animationData={LoginAnimation} />
         </div>
 
-      <div style={{
-        padding: "60px",
-        borderRadius: "8px",
-        backgroundColor: "white",
-        boxShadow: "1px 4px 6px rgba(0, 0, 0, 0.5)",
-        width: "450px",
-        textAlign: "center",
-      }}>
-        <h1 style={{ fontSize: "22px", fontWeight: "600",transition: "color 0.3s", 
-            cursor: "pointer", }} 
-          onMouseEnter={(e) => e.target.style.color = "#1A5BB8"} 
-          onMouseLeave={(e) => e.target.style.color = "black"} // Revert color on hover out
-          >Welcome Back ...!</h1>
-        <p>Enjoy your learning experience and continue where you left off.</p>
-
-        <input
-          type="email"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
+        <div
           style={{
-            width: "100%",
-            padding: "10px",
-            marginBottom: "10px",
-            border: "1px solid #ccc",
-            borderRadius: "4px",
-          }}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-          style={{
-            width: "100%",
-            padding: "10px",
-            marginBottom: "20px",
-            border: "1px solid #ccc",
-            borderRadius: "4px",
-          }}
-        />
-        <button
-          onClick={handleLogin}
-          style={{
-            width: "100%",
-            padding: "10px",
-            backgroundColor: "#7AB2D3",
-            color: "white",
-            border: "none",
-            borderRadius: "4px",
-            cursor: "pointer",
+            padding: "60px",
+            borderRadius: "8px",
+            backgroundColor: "white",
+            boxShadow: "1px 4px 6px rgba(0, 0, 0, 0.5)",
+            width: "450px",
+            textAlign: "center",
           }}
         >
-          Login
-        </button>
-        <p>Don't have an account? ... 
-          <Link href="/register" style={{ color: "#024CAA", textDecoration: "none" }}>
-            Start a new journey
-          </Link>
-        </p>      </div>
-    </div>
+          <h1
+            style={{
+              fontSize: "22px",
+              fontWeight: "600",
+              transition: "color 0.3s",
+              cursor: "pointer",
+            }}
+            onMouseEnter={(e) => (e.target.style.color = "#1A5BB8")}
+            onMouseLeave={(e) => (e.target.style.color = "black")} // Revert color on hover out
+          >
+            Welcome Back ...!
+          </h1>
+          <p>Enjoy your learning experience and continue where you left off.</p>
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            style={{
+              width: "100%",
+              padding: "10px",
+              marginBottom: "10px",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+            }}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            style={{
+              width: "100%",
+              padding: "10px",
+              marginBottom: "20px",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+            }}
+          />
+          <button
+            onClick={handleLogin}
+            style={{
+              width: "100%",
+              padding: "10px",
+              backgroundColor: "#7AB2D3",
+              color: "white",
+              border: "none",
+              borderRadius: "4px",
+              cursor: "pointer",
+            }}
+          >
+            Login
+          </button>
+          <p>
+            Don't have an account? ...
+            <Link
+              href="/register"
+              style={{ color: "#024CAA", textDecoration: "none" }}
+            >
+              Start a new journey
+            </Link>
+          </p>{" "}
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -1,32 +1,166 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-
+import React, { useState } from "react";
+import axios from "axios";
+import "../app/globals.css";
+import Navbar from "../components/Navbar";
+import Link from "next/link";
+import signUpImage from "../../public/signup.png";
 export default function Register() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [role, setRole] = useState('student');
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("student");
 
   const handleRegister = async () => {
+    console.log("clicked");
     try {
-      await axios.post('http://localhost:3000/auth/register', { name, email, password, role });
-      alert('Registration successful!');
+      await axios.post("http://localhost:3000/auth/register", {
+        fName,
+        lName,
+        email,
+        password,
+        role,
+      });
+      alert("Registration successful!");
     } catch (error) {
-      alert('Registration failed: ' + error.response.data.message);
+      alert("Registration failed: " + error.response.data.message);
     }
   };
 
   return (
     <div>
-      <h1>Register</h1>
-      <input type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} />
-      <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-      <select onChange={(e) => setRole(e.target.value)}>
-        <option value="student">Student</option>
-        <option value="instructor">Instructor</option>
-      </select>
-      <button onClick={handleRegister}>Register</button>
+      <Navbar />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "30px",
+        }}
+      >
+        <img
+          src="/signup.png"
+          alt="image"
+          style={{
+            width: "50%",
+            height: "300px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        ></img>
+        <h1
+          style={{
+            color: "#7F8081",
+            fontSize: "20px",
+            fontFamily: "sans-serif",
+          }}
+        >
+          "Join our community and unlock a world of knowledge—create your
+          account to start learning, growing, and achieving your goals today!"
+        </h1>
+        <div
+          style={{
+            padding: "30px",
+            borderRadius: "8px",
+            backgroundColor: "white",
+            boxShadow: "1px 4px 6px rgba(0, 0, 0, 0.5)",
+            width: "450px",
+            textAlign: "center",
+            gap: "20px",
+          }}
+        >
+          <h2
+            style={{
+              fontWeight: "500",
+              fontSize: "20px",
+              marginBottom: "29px",
+            }}
+          >
+            Sign Up!
+          </h2>
+          <div style={{ display: "flex", flexDirection: "row", gap: "6px" }}>
+            <input
+              type="text"
+              placeholder="First Name"
+              onChange={(e) => setFName(e.target.value)}
+              style={{
+                width: "100%",
+                padding: "10px",
+                marginBottom: "10px",
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              onChange={(e) => setLName(e.target.value)}
+              style={{
+                width: "100%",
+                padding: "10px",
+                marginBottom: "10px",
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+              }}
+            />
+          </div>
+
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            style={{
+              width: "100%",
+              padding: "10px",
+              marginBottom: "10px",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+            }}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            style={{
+              width: "100%",
+              padding: "10px",
+              marginBottom: "20px",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+            }}
+          />
+          <select
+            style={{
+              width: "100%",
+              padding: "10px",
+              marginBottom: "20px",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+            }}
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+          >
+            <option value="student">Student</option>
+            <option value="teacher">Teacher</option>
+          </select>
+          <button
+            onClick={handleRegister}
+            style={{
+              width: "100%",
+              padding: "10px",
+              backgroundColor: "#7AB2D3",
+              color: "white",
+              border: "none",
+              borderRadius: "4px",
+              cursor: "pointer",
+            }}
+          >
+            SignUp
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
